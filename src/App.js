2531beb48ca40c4b23665a53b764f961f3755b9f
sumanitian <sumanprasad26042001@gmail.com>
2021-07-17 18:11:45 +0530

    save the name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     //BEM class naming convention
     <div className="app">
       <Header />
+      <TinderCards />
     </div>
   );
 }
--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./TinderCard.css";
 
 function TinderCards() {
+    //store the people
+    const [people, setPeople] = useState([
+        {
+            name: 'Elon Musk',
+            url: "https://m.media-amazon.com/images.M/MV5BMTI5NDY5NjU3NF5BMl5BanBnXkFtZTcwMzQ0MTMyMw@@._V1_UY1200_CR94,0,630,1200_AL_.jpg",
+        },
+    ]);
     return (
-        <div>
-            
+        <div className="tinderCards">
+            {people.map((person) => (
+                <h1>{person.name}</h1>
+            ))}
         </div>
     )
 }
