904b3b4d9d51e7e8c8121a81dd2038a62f761a01
sumanitian <sumanprasad26042001@gmail.com>
2021-07-17 16:01:19 +0530

    created header everything

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     //BEM class naming convention
     <div className="app">
+    <Header />
       <h1>clone the tinder clone</h1>
     </div>
   );
